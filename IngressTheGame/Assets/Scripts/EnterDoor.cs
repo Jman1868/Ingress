dc002d5631cd4ba8ab3a25673eb1cb3d2564126a
FrostDragonGames <43161576+Jman1868@users.noreply.github.com>
2022-02-23 03:33:12 -0800

    Setup basic scene loader.
    Setup basic scene loader that loads on collison. Added asset for skybox and installed probuilder. Changed build to hmtl5.

---
+++ IngressTheGame/Assets/Scripts/EnterDoor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterDoor : MonoBehaviour
+{
+    [SerializeField] private bool fakeDoor;
+    [SerializeField] private Transform teleportEnd; //respawn point
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    /// This funciton teleports or progess to the next scene
+    /// depending on our players choice. 
+    /// If we want to teleport we need to temp disable the character
+    /// controller and then re-enable it.
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.tag == "Player" && fakeDoor == false) //Real door
+		{
+            print("inside");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //load next scene in index
+        }
+        else if (other.gameObject.tag == "Player" && fakeDoor == true) //Fake door
+        {
+            var charControl = other.gameObject.GetComponent <CharacterController>(); //Get component to disable
+            charControl.enabled = false; //disable it
+            other.gameObject.transform.position = teleportEnd.position; //teleport our player
+            charControl.enabled = true; //re-enable it
+            print("wrong door");
+		}
+	}
+
+}
